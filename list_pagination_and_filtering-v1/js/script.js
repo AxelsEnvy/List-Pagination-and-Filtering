f12a2431ac843af6b5232a5f1a941a42e29dd920
Axel Valentine <npvalentine123@gmail.com>
2019-01-19 00:16:30 -0800

    Added a chunk of code
    Added a function for an array of arrays for student information.

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -2,49 +2,25 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+const parent = document.getElementsByClassName('page')[0];
+const students = document.getElementsByClassName('student-item');
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// Function that establishes an array of arrays for student information. Each Child array has ten or less elements attacted to them.
+function arrayOfArrays(students) {
+    const tens = Math.ceil(students.length / 10);
+    let parentArray = [];
+    let bottom = 0;
+    for (let i = 1; i <= tens; i++) {
+        const top = i * 10;
+        let childArray = [];
+        for (let j = bottom; j < top; j++) {
+            if (j < students.length) {
+                childArray.push(students[j]);
+            }
+        }
+        bottom = bottom + 10;
+        parentArray.push(childArray);
+    }
+    return(parentArray);
+}
